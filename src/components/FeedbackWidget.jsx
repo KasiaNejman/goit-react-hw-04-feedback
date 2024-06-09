7ea1b428aa9667aa63a319ffa7b26670baaf791f
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-06-09 15:53:01 +0200

    Create FeedbackWidget.jsx

---
+++ src/components/FeedbackWidget.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import Section from './Section';
+import Statistics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
+import Notification from './Notification';
+
+const FeedbackWidget = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleFeedback = type => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1,
+    }));
+  };
+
+  const { good, neutral, bad } = feedback;
+  const total = good + neutral + bad;
+  const positivePercentage = total > 0 ? Math.round((good / total) * 100) : 0;
+
+  return (
+    <div>
+      <h1>Please leave feedback</h1>
+
+      <Section title="Feedback Options">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
+
+export default FeedbackWidget;
